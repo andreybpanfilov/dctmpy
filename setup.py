8109ac82b9875674dacf7d3d9048affcc1a842eb
andreybpanfilov <andrew@panfilov.tel>
2014-11-21 09:42:02 +1100

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name='dctmpy',
-    version='0.2.1.1',
+    version='0.2.2',
     packages=['dctmpy', 'dctmpy.net', 'dctmpy.obj', 'dctmpy.rpc', 'dctmpy.nagios'],
     package_dir={'': 'src'},
     license='ZPL-2.1',
